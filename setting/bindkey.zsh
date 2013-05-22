bd0dd10ea1cb83b5faa9eeb69b88a42362444f1e
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-22 22:54:34 +0900

    Provide zvim-dirmark feature

--- setting/bindkey.zsh
+++ setting/bindkey.zsh
@@ -5,3 +5,7 @@ bindkey -v
 bindkey -M vicmd 'Y' vi-yank-eol
 bindkey -M vicmd 'u' undo
 bindkey -M vicmd '^r' redo
+
+# Directory Mark bindings
+bindkey -M vicmd 'm' zvim-dirmark-add
+bindkey -M vicmd '`' zvim-dirmark-jump
--- setting/widget.zsh
+++ setting/widget.zsh
@@ -7,3 +7,14 @@ do
 	autoload -U $w:t
 	zle -N $w:t
 done
+
+# Prepare required widgets
+autoload -U read-from-minibuffer
+
+# Prepare global variables
+typeset -A zvim_dirmarks
+mkdir -p $zvimdir/save
+zvim_dirmarks_save_file=$zvimdir/save/dirmarks
+
+# Load global variables
+zvim-dirmark-load
