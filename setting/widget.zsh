22f04c6f75eccf744ff052e21d7912245873d438
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-01-23 14:17:21 +0900

    Use ~/.zvim/save as default save directory

--- setting/widget.zsh
+++ setting/widget.zsh
@@ -13,8 +13,9 @@ autoload -U read-from-minibuffer
 
 # Prepare global variables
 typeset -A zvim_dirmarks
-mkdir -p $zvimdir/save
-zvim_dirmarks_save_file=$zvimdir/save/dirmarks
+zvim_savedir=${zvim_savedir:-~/.zvim/save}
+mkdir -p $zvim_savedir
+zvim_dirmarks_save_file=$zvim_savedir/dirmarks
 
 # Load global variables
 zvim-dirmark-load
